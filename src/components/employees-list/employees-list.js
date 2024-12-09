8f24fdcc9e794e802712702794a0febdb6f12248
Maksim Shubny <shubny.maksim@yandex.ru>
2024-12-10 02:12:03 +0500

    add like for employees-list-item

--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -2,14 +2,13 @@ import EmployeeslistItem from '../employees-list-item/employees-list-item';
 
 import './employees-list.css';
 
-const Employeeslist = () => {
-  return (
-    <ul className="app-list list-group">
-      <EmployeeslistItem />
-      <EmployeeslistItem />
-      <EmployeeslistItem />
-    </ul>
-  );
+const Employeeslist = ({ data }) => {
+  const elements = data.map((item) => {
+    const { id, ...itemProps } = item;
+    return <EmployeeslistItem key={id} {...itemProps} />;
+  });
+
+  return <ul className="app-list list-group">{elements}</ul>;
 };
 
 export default Employeeslist;
